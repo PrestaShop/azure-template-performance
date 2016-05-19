8d1b23acbbe41c430835b35f0671d08d59eff6c2
HeL <herve.leclerc@alterway.fr>
2016-05-19 19:33:31 +0200

    le dernier d-du soir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ function usage()
 error_log()
 {
     if [ "$?" != "0" ]; then
-        log "$1" "1"
+        log "$1"
         log "Deployment ends with an error" "1"
         exit 1
     fi
@@ -17,40 +17,101 @@ error_log()
 function log()
 {
 	
-  mess="$(date) - $(hostname): $1"
+  mess="$(hostname): $1"
 
   logger -t "${BASH_SCRIPT}" "${mess}"
-   
-  echo "$(date) : $1"
+
 }
 
 function ssh_config()
 {
-  # log "tld is ${tld}"
-  log "Configure ssh..." "0"
-  
-  log "Create ssh configuration for ${ANSIBLE_USER}" "0"
+  log "Configure ssh..."
+  log "Create ssh configuration for ${ANSIBLE_USER}"
   cat << 'EOF' >> /home/${ANSIBLE_USER}/.ssh/config
 Host *
-    user devops
+    user ${ANSIBLE_USER}
     StrictHostKeyChecking no
 EOF
   error_log "Unable to create ssh config file for user ${ANSIBLE_USER}"
+  
+  log "Copy generated keys..."
+  
+  cp id_rsa "/home/${ANSIBLE_USER}/.ssh/id_rsa"
+  error_log "Unable to copy id_rsa key to $ANSIBLE_USER .ssh directory"
 
+  cp id_rsa.pub "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub"
+  error_log "Unable to copy id_rsa.pub key to $ANSIBLE_USER .ssh directory"
+  
+  cat "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub" >> "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
+  error_log "Unable to copy $ANSIBLE_USER id_rsa.pub to authorized_keys "
+
+  chmod 700 "/home/${ANSIBLE_USER}/.ssh"
+  error_log "Unable to chmod $ANSIBLE_USER .ssh directory"
+
+  chown -R "${ANSIBLE_USER}:" "/home/${ANSIBLE_USER}/.ssh"
+  error_log "Unable to chown to $ANSIBLE_USER .ssh directory"
+
+  chmod 400 "/home/${ANSIBLE_USER}/.ssh/id_rsa"
+  error_log "Unable to chmod $ANSIBLE_USER id_rsa file"
+
+  chmod 644 "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub"
+  error_log "Unable to chmod $ANSIBLE_USER id_rsa.pub file"
+
+  chmod 400 "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
+  error_log "Unable to chmod $ANSIBLE_USER authorized_keys file"
+  
 }
 
 function install_ansible()
 {
-    apt-get --yes --force-yes install software-properties-common
-    apt-add-repository ppa:ansible/ansible
-    apt-get --yes --force-yes update
-    apt-get --yes --force-yes install ansible
-    # install sshpass
-    apt-get --yes --force-yes install sshpass
-    # install Git
-    apt-get --yes --force-yes install git
-    # install python
-    apt-get --yes --force-yes install python-pip
+    log "Install software-properties-common ..."
+    until apt-get --yes install software-properties-common
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+    
+    log "Install ppa:ansible/ansible ..."
+    until apt-add-repository ppa:ansible/ansible
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+    
+    log "Update System ..."
+    until apt-get --yes update
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+    
+    log "Install Ansible ..."
+    until apt-get --yes install ansible
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+    
+    log "Install sshpass"
+    until apt-get --yes install sshpass
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+   
+    log "Install git ..."
+    until apt-get --yes install git
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
+    
+    log "Install pip ..."
+    until apt-get --yes install python-pip
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
 }
 
 
@@ -61,25 +122,46 @@ function generate_sshkeys()
 
 function put_sshkeys()
  {
-    log "INFO:Retrieving ssh keys from Azure Storage"
+   
+    log "Install azure storage python module ..."
     pip install azure-storage
 
-    # Download both Private and Public Key
+    # Push both Private and Public Key
+    log "Push ssh keys to Azure Storage"
     python WriteSSHToPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" id_rsa
     python WriteSSHToPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" id_rsa.pub
 
 }
 
+function fix_etc_hosts()
+{
+  log "Add hostame and ip in hosts file ..."
+  IP=$(ip addr show eth0 | grep inet | grep -v inet6 | awk '{ print $2; }' | sed 's?/.*$??')
+  HOST=$(hostname)
+  echo "${IP}" "${HOST}" >> /etc/hosts
+}
+
+log "Execution of Install Script from CustomScript ..."
 
 ## Variables
+
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+log "CustomScript Directory is ${CWD}"
+
 BASH_SCRIPT="${0}"
 STORAGE_ACCOUNT_NAME="${1}"
 STORAGE_ACCOUNT_KEY="${2}"
 ANSIBLE_USER="${3}"
 
-##
 
-ssh_config
+
+##
+fix_etc_hosts
 install_ansible
 generate_sshkeys
+ssh_config
 put_sshkeys
+
+
+log "End of Execution of Install Script from CustomScript ..."
