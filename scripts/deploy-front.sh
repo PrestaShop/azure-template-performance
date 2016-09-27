7d234d95486100d44c4850e6be4a9b0642c5eae9
HeL <herve.leclerc@alterway.fr>
2016-09-27 19:28:53 +0200

    version1.7.8

--- scripts/deploy-front.sh
+++ scripts/deploy-front.sh
@@ -99,8 +99,14 @@ function ssh_config_root()
 
 function install_packages()
 {
+    log "Update System ..."
+    until apt-get --yes update
+    do
+      log "Lock detected on apt-get while install Try again..."
+      sleep 2
+    done
     log "Install software-properties-common ..."
-    until apt-get --yes install software-properties-common
+    until apt-get --yes install software-properties-common build-essential libssl-dev libffi-dev python-dev
     do
       log "Lock detected on apt-get while install Try again..."
       sleep 2
